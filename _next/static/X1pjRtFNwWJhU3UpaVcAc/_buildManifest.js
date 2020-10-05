02b5bd2aa110a6071bb93e46572da3695a0d0b66
Matt Huggins <matt.huggins@gmail.com>
2020-10-04 23:39:04 -0500

    Updates

---
+++ _next/static/X1pjRtFNwWJhU3UpaVcAc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-c83f63465eee98be7f71.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cb301e4e9e10851ffa77.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
