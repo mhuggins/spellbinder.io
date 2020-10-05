0b3a72b1293acac288bb0f46b24a2e2ed4f0bfca
Matt Huggins <matt.huggins@gmail.com>
2020-10-05 14:12:37 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "06f0a81039476c2134b0";
+/******/ 	var hotCurrentHash = "e72ae50bfc49283de036";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
