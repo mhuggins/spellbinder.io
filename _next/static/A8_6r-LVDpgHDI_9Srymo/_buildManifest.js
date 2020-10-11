e1f7a88be3436bfd6dbee1e0447b7756dad15e30
Matt Huggins <matt.huggins@gmail.com>
2020-10-11 16:14:59 -0500

    Updates

---
+++ _next/static/A8_6r-LVDpgHDI_9Srymo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-4eb4ed374cc0e31659cb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cb301e4e9e10851ffa77.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
